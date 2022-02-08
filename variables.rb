8cfc0a57f061a3e2ea7c74b5fcd04556f69d1ef1
Andrew King <dev.andrewking@gmail.com>
2022-02-07 21:21:59 -0600

    add annotations

--- variables.rb
+++ variables.rb
@@ -1,10 +1,13 @@
+# assign names to string variable
 first_name = "Andrew"
 last_name = "King"
 initial = "S"
 
+# get length of string, which is an integer, assign to variable
 letters_in_name = first_name.length
 age = 28
 
+# assign floats to variables
 pi_rounded = 3.14
 account_balance = 2000.62
 
